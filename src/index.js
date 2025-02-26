43c010931ff78f3e1ca6b990bd592a7e23426331
Sameer <sameersingh192004@gmail.com>
2025-02-26 19:44:10 +0530

    Added flights model with foreign key constraint defined during model definition

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const {ServerConfig , Logger} = require('./config');
 const apiRoutes = require('./routes');
-
+ 
 const app = express();
 
 app.use(express.json());
@@ -11,10 +11,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', apiRoutes)
 
-app.listen(ServerConfig.PORT , async () => {
+app.listen(ServerConfig.PORT , () => {
     console.log(`Server running on port ${ServerConfig.PORT}`);
 
+    
     // sequelize provides some methods to interact with the database like create, findAll, findByPk, destroy, etc.
+
     // const {City , Airport} = require('./models');
     // const cities = await City.findAll({});
     // console.log(cities);
