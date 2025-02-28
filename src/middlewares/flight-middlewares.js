862ff3b4416d9af01d817b897578753ecb791556
Sameer <sameersingh192004@gmail.com>
2025-02-28 18:29:38 +0530

    fix: fixed the createFlight middleware

--- src/middlewares/flight-middlewares.js
+++ src/middlewares/flight-middlewares.js
@@ -3,7 +3,12 @@ const {StatusCodes} = require('http-status-codes');
 const {ErrorResponse} = require('../utils/common');
 const AppError = require('../utils/errors/app-error');
 
+
 function validateCreateRequest(req , res, next) {
+    const departureTime = new Date(req.body.departureTime);
+    const arrivalTime = new Date(req.body.arrivalTime);
+
+    
     if(!req.body.flightNumber){
         ErrorResponse.message = "Something went wrong while creating flight";
         ErrorResponse.error = new AppError(['flightNumber not found in the incoming request in the correct form'], StatusCodes.BAD_REQUEST);
@@ -11,6 +16,7 @@ function validateCreateRequest(req , res, next) {
                 .status(StatusCodes.BAD_REQUEST) 
                 .json(ErrorResponse);
     }
+
     if(!req.body.airplaneId){
         ErrorResponse.message = "Something went wrong while creating flight";
         ErrorResponse.error = new AppError(['airplaneId not found in the incoming request in the correct form'], StatusCodes.BAD_REQUEST);
@@ -18,6 +24,7 @@ function validateCreateRequest(req , res, next) {
                 .status(StatusCodes.BAD_REQUEST) 
                 .json(ErrorResponse);
     }
+
     if(!req.body.departureAirportId){
         ErrorResponse.message = "Something went wrong while creating flight";
         ErrorResponse.error = new AppError(['departureAirportId not found in the incoming request in the correct form'], StatusCodes.BAD_REQUEST);
@@ -25,6 +32,7 @@ function validateCreateRequest(req , res, next) {
                 .status(StatusCodes.BAD_REQUEST)
                 .json(ErrorResponse)
     }
+
     if(!req.body.arrivalAirportId){
         ErrorResponse.message = "Something went wrong while creating flight";
         ErrorResponse.error = new AppError(['arrivalAirportId not found in the incoming request in the correct form'], StatusCodes.BAD_REQUEST);
@@ -32,6 +40,7 @@ function validateCreateRequest(req , res, next) {
                 .status(StatusCodes.BAD_REQUEST)
                 .json(ErrorResponse)
     }
+
     if(!req.body.arrivalTime){
         ErrorResponse.message = "Something went wrong while creating flight";
         ErrorResponse.error = new AppError(['arrivalTime not found in the incoming request in the correct form'], StatusCodes.BAD_REQUEST);
@@ -39,6 +48,7 @@ function validateCreateRequest(req , res, next) {
                 .status(StatusCodes.BAD_REQUEST)
                 .json(ErrorResponse)
     }
+
     if(!req.body.departureTime){
         ErrorResponse.message = "Something went wrong while creating flight";
         ErrorResponse.error = new AppError(['departureTime not found in the incoming request in the correct form'], StatusCodes.BAD_REQUEST);
@@ -46,6 +56,7 @@ function validateCreateRequest(req , res, next) {
                 .status(StatusCodes.BAD_REQUEST)
                 .json(ErrorResponse)
     }
+
     if(!req.body.price){
         ErrorResponse.message = "Something went wrong while creating flight";
         ErrorResponse.error = new AppError(['price not found in the incoming request in the correct form'], StatusCodes.BAD_REQUEST);
@@ -53,7 +64,7 @@ function validateCreateRequest(req , res, next) {
                 .status(StatusCodes.BAD_REQUEST)
                 .json(ErrorResponse)
     }
-    
+
     if(!req.body.totalSeats){
         ErrorResponse.message = "Something went wrong while creating flight";
         ErrorResponse.error = new AppError(['totalSeats not found in the incoming request in the correct form'], StatusCodes.BAD_REQUEST);
@@ -61,7 +72,8 @@ function validateCreateRequest(req , res, next) {
                 .status(StatusCodes.BAD_REQUEST)
                 .json(ErrorResponse)
     }
-    if(req.body.departureTime > req.body.arrivalTime){
+
+    if(departureTime.getTime() > arrivalTime.getTime()){
         ErrorResponse.message = "Something went wrong while creating flight";
         ErrorResponse.error = new AppError(['departureTime cannot be more than arrivalTime'], StatusCodes.BAD_REQUEST);
         return res
